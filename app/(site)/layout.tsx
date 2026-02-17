2b1301fdb4570cf656b13cea7b5b7e61c495eb40
Davide Lista <d.lista@mokkastudios.com>
2026-02-17 17:06:48 +0100

    add iubend

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -11,6 +11,10 @@ export default function SiteLayout({
       <Header />
       <main className="min-h-screen">{children}</main>
       <Footer />
+      <script
+        type="text/javascript"
+        src="https://embeds.iubenda.com/widgets/49b529b0-feb4-4165-8259-e7fa98fd317f.js"
+      ></script>
     </>
   );
 }
