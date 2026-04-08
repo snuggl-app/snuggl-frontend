45019a9283f4a006aefb32b4746920ebdb3cb5ee
Davide Lista <d.lista@mokkastudios.com>
2026-04-08 15:52:56 +0200

    add redirect for form

---
+++ app/(site)/sondaggio/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Sondaggio() {
+  useEffect(() => {
+    window.location.href = "https://forms.gle/1g7xzPWj1RLgB5R68";
+  }, []);
+
+  return null;
+}
--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/sondaggio",
-        destination: "https://forms.gle/1g7xzPWj1RLgB5R68",
-        permanent: false,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
