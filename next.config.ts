58d915cb3ca162166554ca2244b521c54aaed1a5
Davide Lista <d.lista@mokkastudios.com>
2026-04-08 15:45:28 +0200

    add redirect for form

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/sondaggio",
+        destination: "https://forms.gle/1g7xzPWj1RLgB5R68",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
